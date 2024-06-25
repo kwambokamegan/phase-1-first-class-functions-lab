695cc0108591bc30887b52d2567700ba0a136dbc
Megan <megan.nyakina@student.moringaschool.com>
2024-06-25 16:08:55 +0300

    adds first class fundamentals lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution in this file!
+
+ //returnFirstTwoDrivers
+
+const returnFirstTwoDrivers = function(drivers)
+
+{return drivers.slice(0,2);};
+
+
+const drivers = ['Antonia' , 'Nuru' , 'Amari' , 'Mo'];
+
+console.log(returnFirstTwoDrivers(drivers));
+
+
+
+//retuenLastTwoDrivers
+
+const returnLastTwoDrivers = function(drivers)
+
+{return drivers.slice(-2);};
+
+ 
+//selecting Drivers
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+//createFareMultiplier
+
+const createFareMultiplier =
+
+function(multiplier)
+
+{ return function(fare)
+
+    {return fare *multiplier;};
+};
+
+
+//fareDoubler
+
+const fareDoubler = createFareMultiplier (2)
+
+
+//fareTripler
+
+const fareTripler =createFareMultiplier (3)
+
+
+//selectDifferentDrivers
+
+ const selectDifferentDrivers = function(drivers, selectFunction)
+ 
+ {return selectFunction(drivers);};
+
+
